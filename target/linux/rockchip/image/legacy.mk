193e2ebda0a6a2538b81396c3f5acf891746fa9e
Liangbin Lian <jjm2473@gmail.com>
2025-02-05 19:37:32 +0800

    target/rockchip: r6s firmware supports r6c

--- target/linux/rockchip/image/legacy.mk
+++ target/linux/rockchip/image/legacy.mk
@@ -3,6 +3,8 @@ $(call Device/Legacy)
   DEVICE_VENDOR := FriendlyARM
   DEVICE_MODEL := NanoPi R6S
   SOC := rk3588s
+  SUPPORTED_DEVICES += friendlyarm,nanopi-r6s friendlyarm,nanopi-r6c
+  SUPPORTED_DEVICES += friendlyelec,nanopi-r6s friendlyelec,nanopi-r6c
   DEVICE_DTS := rockchip/rk3588s-nanopi-r6s rockchip/rk3588s-nanopi-r6c
   BOOT_SCRIPT := rk3588-friendlyelec
   UBOOT_DEVICE_NAME := easepi-rk3588
