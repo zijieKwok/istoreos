ff7754c8af3f47973c7064ea49fe57ce11fba063
Liangbin Lian <jjm2473@gmail.com>
2025-02-05 16:45:14 +0800

    target/rockchip: supports boot from legacy uboot

---
+++ target/linux/rockchip/image/legacy.mk
@@ -0,0 +1,9 @@
+define Device/friendlyarm_nanopi-r6s
+$(call Device/Legacy)
+  DEVICE_VENDOR := FriendlyARM
+  DEVICE_MODEL := NanoPi R6S
+  SOC := rk3588s
+  DEVICE_DTS := rockchip/rk3588s-nanopi-r6s rockchip/rk3588s-nanopi-r6c
+  BOOT_SCRIPT := rk3588-friendlyelec
+  DEVICE_PACKAGES := kmod-r8169
+endef
