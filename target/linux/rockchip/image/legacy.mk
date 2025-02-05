9b5268d1ccfc2b2985e1fb82ef85fbd1800932ca
Liangbin Lian <jjm2473@gmail.com>
2025-02-05 16:47:36 +0800

    target/rockchip: r6s use generic uboot

--- target/linux/rockchip/image/legacy.mk
+++ target/linux/rockchip/image/legacy.mk
@@ -5,5 +5,6 @@ $(call Device/Legacy)
   SOC := rk3588s
   DEVICE_DTS := rockchip/rk3588s-nanopi-r6s rockchip/rk3588s-nanopi-r6c
   BOOT_SCRIPT := rk3588-friendlyelec
+  UBOOT_DEVICE_NAME := easepi-rk3588
   DEVICE_PACKAGES := kmod-r8169
 endef
